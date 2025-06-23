f739d94abea7e9f25fa3fbfad3d5f557cd5f70d8
prerna <prernapatil1308@gmail.com>
2025-06-23 12:40:38 +0530

    fifth commoit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import CoursesPage from "./pages/CoursesPage";
+import AdmissionsPage from "./pages/AdmissionsPage";
+import Home from "./pages/Home";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
+
+
+
+
+const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <>
+      {showPopup && (
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Prerna Ganpati Patil"
+          studentPhotoUrl="\images\prerna1.jpg"
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills."
+        />
+      )}
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/aboutpage" element={<AboutPage />} />
+          <Route path="/coursespage" element={<CoursesPage />} />
+          <Route path="/contactpage" element={<ContactPage />} />
+          <Route path="/admissionspage" element={<AdmissionsPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
